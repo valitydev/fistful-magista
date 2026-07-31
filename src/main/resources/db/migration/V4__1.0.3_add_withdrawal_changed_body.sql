d7dd956cccdca125b156c0ac2a3222d959edd3f8
Egor Cherniak <cherniak3@yandex.ru>
2026-07-31 14:39:55 +0300

    Feedback edits

--- src/main/resources/db/migration/V4__1.0.3_add_withdrawal_changed_body.sql
+++ src/main/resources/db/migration/V4__1.0.3_add_withdrawal_changed_body.sql
@@ -1,3 +1,3 @@
 ALTER TABLE mst.withdrawal_data
-    ADD COLUMN changed_amount BIGINT,
-    ADD COLUMN changed_currency_code CHARACTER VARYING;
+    ADD COLUMN IF NOT EXISTS changed_amount BIGINT,
+    ADD COLUMN IF NOT EXISTS changed_currency_code CHARACTER VARYING;
